6c7116480a615bf2d17e69a3e1d73cdedc6577ef
Joey <joeynap@hotmail.com>
2017-12-19 20:34:51 +0100

    opmaak verbeterd

--- src/CategorieFrame.py
+++ src/CategorieFrame.py
@@ -11,53 +11,65 @@ class frm_notities(wx.Frame):
         wx.Frame.__init__(self, None, -1, 'Overzicht scherm',   
                 size=(800, 800))  
         pnl_mainPanel = wx.Panel(self, -1)
-        #texts
-        ##scherm titel
-        self.txt_titel = wx.StaticText(pnl_mainPanel, -1, "Categorieën")
-        fnt_titel = wx.Font(18, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL
-                            , wx.FONTWEIGHT_BOLD)
-        self.txt_titel.SetFont(fnt_titel)
-        ##versienummer
-        self.txt_versie = wx.StaticText(pnl_mainPanel, -1, "versie 0.0.1")
-        ##tijdelijk categorielijst
-        self.txt_categorie = wx.StaticText(pnl_mainPanel, -1,
-                                           "Hier komen categorieën")
-        #invoer
-        ##zoekbalk
+
+#fonts
+        fnt_titel = wx.Font(18, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+                            wx.FONTWEIGHT_BOLD)
+        fnt_default = wx.Font(10, wx.FONTFAMILY_DEFAULT,
+                              wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+
+#creeër functies
+        def textMaker(TM_textLabel, TM_font=fnt_default):
+            txt_staticText = wx.StaticText(pnl_mainPanel, -1, TM_textLabel)
+            txt_staticText.SetFont(TM_font)
+            return txt_staticText
+
+        def buttonMaker(BM_btnLabel, BM_evtNaam, BM_lblFont=fnt_default):
+            btn_button = wx.Button(pnl_mainPanel, -1, BM_btnLabel)
+            self.Bind(wx.EVT_BUTTON, BM_evtNaam, btn_button)
+            btn_button.SetFont(BM_lblFont)
+            btn_button.SetDefault()
+            return btn_button
+
+        def boxMaker(BxM_itemList, BxM_size, BxM_flagList,
+                     BxM_rotatie=wx.HORIZONTAL,
+                     BxM_borderList=False):
+            if BxM_borderList == False:
+                BxM_borderList = [1]*len(BxM_itemList)
+            box_newSizer = wx.BoxSizer(BxM_rotatie)
+            for Pos in range(0, len(BxM_itemList)):
+                box_newSizer.Add(BxM_itemList[Pos], BxM_size[Pos],
+                                 flag=BxM_flagList[Pos],
+                                 border=BxM_borderList[Pos])
+            return box_newSizer
+
+#texts
+        self.txt_titel = textMaker("Categorieën", fnt_titel)
+        self.txt_versie = textMaker("versie 0.0.1")
+        self.txt_categorie = textMaker("Hier komen categorieën")#tijdelijk
+        
         self.zoekBalk = wx.TextCtrl(pnl_mainPanel, -1,
                                     value="Zoeken in categorieën",
                                     size=(200, 50),
                                     name="zoekterm")
-        #buttons
-        ##instellingen
-        self.btn_instelling = wx.Button(pnl_mainPanel, -1, "Instellingen")  
-        self.Bind(wx.EVT_BUTTON, self.instellingenKnop, self.btn_instelling)  
-        self.btn_instelling.SetDefault()
-        ##beheren
-        self.btn_beheren = wx.Button(pnl_mainPanel, -1, "Beheren")
-        self.Bind(wx.EVT_BUTTON, self.beherenKnop, self.btn_beheren)  
-        self.btn_beheren.SetDefault()
-        #boxes
-        ##titel en zoekbalk
-        box_titelZoeken = wx.BoxSizer(wx.HORIZONTAL)
-        box_titelZoeken.Add(self.txt_titel, 10, flag=wx.ALL)
-        box_titelZoeken.Add((0, 0), 50, flag=wx.ALL)
-        box_titelZoeken.Add(self.zoekBalk, 10, flag=wx.ALL)
-        ##categorie lijst
-        box_categories = wx.BoxSizer(wx.HORIZONTAL)
-        box_categories.Add(self.txt_categorie, flag=wx.ALIGN_CENTER)
-        ##knoppen en versienummer
-        box_btnsVersie = wx.BoxSizer(wx.HORIZONTAL)
-        box_btnsVersie.Add(self.btn_instelling, 10, flag=wx.ALL)
-        box_btnsVersie.Add((0, 0), 20, flag=wx.ALL)
-        box_btnsVersie.Add(self.txt_versie, 10, flag=wx.ALL)
-        box_btnsVersie.Add((0, 0), 20, flag=wx.ALL)
-        box_btnsVersie.Add(self.btn_beheren, 10, flag=wx.ALL)
-        ##alle boxes samen
-        box_mainBox = wx.BoxSizer(wx.VERTICAL)
-        box_mainBox.Add(box_titelZoeken, 10, flag=wx.ALL, border=10)
-        box_mainBox.Add(box_categories, 80, flag=wx.ALL, border=10)
-        box_mainBox.Add(box_btnsVersie, 10, flag=wx.ALL, border=10)
+#buttons
+        self.btn_instelling = buttonMaker("Instellingen",
+                                          self.instellingenKnop)
+        self.btn_beheren = buttonMaker("Beheren", self.beherenKnop)
+#boxes
+        box_titelZoeken = boxMaker([self.txt_titel, (0, 0), self.zoekBalk],
+                                   [10, 50, 10], [wx.ALL, wx.ALL, wx.ALL])
+        box_categories = boxMaker([self.txt_categorie], [10],
+                                  [wx.ALIGN_CENTER])
+        box_btnsVersie = boxMaker([self.btn_instelling, (0, 0),
+                                   self.txt_versie,(0, 0), self.btn_beheren],
+                                  [10, 20, 10, 20, 10],
+                                  [wx.ALL, wx.ALL, wx.ALL, wx.ALL, wx.ALL])
+        box_mainBox = boxMaker([box_titelZoeken, box_categories,
+                                box_btnsVersie], [10, 80, 10],
+                               [wx.ALL,wx.ALL,wx.ALL], wx.VERTICAL,
+                               [10, 10, 10])
+        
         pnl_mainPanel.SetSizer(box_mainBox)
 
     def instellingenKnop(self, event):
