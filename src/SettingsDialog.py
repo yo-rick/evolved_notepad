02d081e68aee715442e6d652d5f22520ce488328
yo-rick <schrijfaan@me.com>
2018-01-09 14:51:57 +0100

    implant of wesleys englisch variable names to my script

--- src/SettingsDialog.py
+++ src/SettingsDialog.py
@@ -19,8 +19,10 @@ Log
 
 """
 import wx
-
+import sys
 import panels
+
+from settings import Settings
 
 
 class SettingsDialog(wx.Dialog):
@@ -28,4 +30,34 @@ class SettingsDialog(wx.Dialog):
     def __init__(self, parent, id):
         super().__init__(parent, id, "Instellingen", size=(450, 420))
         self.panel = panels.SettingsPanel(self, id)
+        self.panel.cbb_button_anu.Bind(wx.EVT_BUTTON, self.cancel)
+        self.panel.cbb_button_ops.Bind(wx.EVT_BUTTON, self.save)
+        self.panel.cpaf_select_opslagmap.Bind(wx.EVT_BUTTON, self.select_Dir)
+        self.settings = Settings.getInstance()
         self.ShowModal()
+
+    def cancel(self, event):
+        self.Destroy() # moet nog gekoppeld worden
+
+    def select_Dir(self, event):
+        print("Select dir")
+        sd_dir = wx.DirDialog(self, "kiez een map:", style=wx.DD_DEFAULT_STYLE)
+        if sd_dir.ShowModal() == wx.ID_OK:
+            sd_get_dir = sd_dir.GetPath()
+            self.settings.setSetting("path", sd_get_dir)
+            self.settings.writeToFile()
+        sd_dir.Destroy()
+    
+    def save(self, event):
+        self.settings.setSetting("prefix", self.panel.cpaf_prefix_textfield.GetValue())
+        self.settings.setSetting("font-family", self.panel.cebl_combo_box.GetValue())
+        self.settings.setSetting("font-size", self.panel.cebr_font_size_spinner.GetValue())
+        self.settings.setSetting("tab-length", self.panel.cebr_tab_spinner.GetValue())
+        self.settings.setSetting("automatic-save", self.panel.cebr_cb.GetValue())
+        self.settings.writeToFile()
+#        print("klik opslaan")
+#        print("prefix", self.panel.cpaf_prefix_textfield.GetValue())
+#        print("Lettertype", self.panel.cebl_combo_box.GetValue())
+#        print("Letterformaat", self.panel.cebr_font_size_spinner.GetValue())
+#        print("tablengte", self.panel.cebr_tab_spinner.GetValue())
+#        print("Auto opslaan", self.panel.cebr_cb.GetValue())
--- src/panels/SettingsPanel.py
+++ src/panels/SettingsPanel.py
@@ -17,6 +17,7 @@ Log
 import wx
 
 import MainFrame
+import sys
 from settings import Settings
 
 
@@ -63,9 +64,9 @@ class SettingsPanel(wx.Panel):
 
     def createPrefixAndFolder(self, id):
         cpaf_prefix_txt = wx.StaticText(self, id, PREFIX)
-        cpaf_prefix_textfield = wx.TextCtrl(self)
+        self.cpaf_prefix_textfield = wx.TextCtrl(self)
         cpaf_save_folder_txt = wx.StaticText(self, id, SAVE_FOLDER)
-        cpaf_select_opslagmap = wx.Button(self, id, SELECT_FOLDER)
+        self.cpaf_select_opslagmap = wx.Button(self, id, SELECT_FOLDER)
         cpaf_vbox_left = wx.BoxSizer(wx.VERTICAL)
         cpaf_vbox_left.AddSpacer(5)
         cpaf_vbox_left.Add(
@@ -75,9 +76,9 @@ class SettingsPanel(wx.Panel):
             cpaf_save_folder_txt, 1, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
         cpaf_vbox_right = wx.BoxSizer(wx.VERTICAL)
         cpaf_vbox_right.Add(
-            cpaf_prefix_textfield, 1, wx.EXPAND | wx.ALIGN_RIGHT)
+            self.cpaf_prefix_textfield, 1, wx.EXPAND | wx.ALIGN_RIGHT)
         cpaf_vbox_right.Add(
-            cpaf_select_opslagmap, 1, wx.EXPAND | wx.ALIGN_RIGHT)
+            self.cpaf_select_opslagmap, 1, wx.EXPAND | wx.ALIGN_RIGHT)
         cpaf_hbox = wx.BoxSizer(wx.HORIZONTAL)
         cpaf_hbox.Add(cpaf_vbox_left, 1, wx.EXPAND)
         cpaf_hbox.AddSpacer(20)
@@ -85,14 +86,14 @@ class SettingsPanel(wx.Panel):
         return cpaf_hbox
 
     def createBottomBox(self, id):
-        cbb_button_anu = wx.Button(self, id, CANCEL)
-        cbb_button_ops = wx.Button(self, id, SAVE)
+        self.cbb_button_anu = wx.Button(self, id, CANCEL)
+        self.cbb_button_ops = wx.Button(self, id, SAVE)
         cbb_version_txt = wx.StaticText(self, id, MainFrame.VERSION_STRING,
                                        style=wx.ALIGN_CENTER)
         cbb_hbox = wx.BoxSizer(wx.HORIZONTAL)
-        cbb_hbox.Add(cbb_button_anu, 1)
+        cbb_hbox.Add(self.cbb_button_anu, 1)
         cbb_hbox.Add(cbb_version_txt, 3, wx.ALIGN_CENTER)
-        cbb_hbox.Add(cbb_button_ops, 1, wx.ALIGN_RIGHT)
+        cbb_hbox.Add(self.cbb_button_ops, 1, wx.ALIGN_RIGHT)
         return cbb_hbox
 
     def createTitle(self, id):
@@ -114,10 +115,12 @@ class SettingsPanel(wx.Panel):
     def createEditBoxLeft(self, id):
         cebl_tab_txt = wx.StaticText(self, id, TAB_LENGTH)
         cebl_save_txt = wx.StaticText(self, id, AUTOMATIC_SAVE)
-        cebl_font_choices = ["een", "twee", "Dit_is_een_lange_fam_naam"]
-        cebl_combo_box = wx.ComboBox(self, choices=cebl_font_choices)
+        cebl_font_choices = ["Default", "Modern", "Roman", "Script",
+                             "Swiss", "Teletype"]
+        self.cebl_combo_box = wx.ComboBox(self, choices=cebl_font_choices)
         cebl_vbox = wx.BoxSizer(wx.VERTICAL)
-        cebl_vbox.Add(cebl_combo_box, 1, wx.EXPAND | wx.ALIGN_CENTRE_VERTICAL)
+        cebl_vbox.Add(self.cebl_combo_box, 1,
+                      wx.EXPAND | wx.ALIGN_CENTRE_VERTICAL)
         cebl_vbox.Add(cebl_tab_txt, 1, wx.EXPAND | wx.ALIGN_CENTRE_VERTICAL)
         cebl_vbox.Add(cebl_save_txt, 1, wx.EXPAND | wx.ALIGN_CENTRE_VERTICAL)
         return cebl_vbox
@@ -125,14 +128,14 @@ class SettingsPanel(wx.Panel):
     def createEditBoxRight(self, id):
         cebr_font_size = self.settings.getSetting('font-size')
         cebr_tab_length = self.settings.getSetting('tab-length')
-        cebr_font_size_spinner = wx.SpinCtrl(self, min=5, max=25,
+        self.cebr_font_size_spinner = wx.SpinCtrl(self, min=5, max=25,
                                              initial=cebr_font_size)
-        cebr_tab_spinner = wx.SpinCtrl(self, min=2, max=8,
+        self.cebr_tab_spinner = wx.SpinCtrl(self, min=2, max=8,
                                        initial=cebr_tab_length)
         cebr_vbox = wx.BoxSizer(wx.VERTICAL)
         expand_center_flag = wx.EXPAND | wx.ALIGN_CENTER_VERTICAL
-        cebr_vbox.Add(cebr_font_size_spinner, 1, expand_center_flag)
-        cebr_vbox.Add(cebr_tab_spinner, 1, expand_center_flag)
-        cebr_cb = wx.CheckBox(self)
-        cebr_vbox.Add(cebr_cb, 1, expand_center_flag)
+        cebr_vbox.Add(self.cebr_font_size_spinner, 1, expand_center_flag)
+        cebr_vbox.Add(self.cebr_tab_spinner, 1, expand_center_flag)
+        self.cebr_cb = wx.CheckBox(self)
+        cebr_vbox.Add(self.cebr_cb, 1, expand_center_flag)
         return cebr_vbox
